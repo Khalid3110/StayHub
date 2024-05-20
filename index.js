ae9932f41c7607d4aa74bf40c5f6f6d59a761186
Khalid Khan <khankhalid00621@gmail.com>
2024-05-20 23:26:53 +0530

    create signup page and authentication

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user.model");
 
 const listings = require("./routes/listing");
 const reviews = require("./routes/review");
+const user = require("./routes/user");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -58,6 +62,13 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -66,6 +77,7 @@ app.use((req, res, next) => {
 
 app.use("/listings", listings);
 app.use("/listings/:id/review", reviews);
+app.use("/", user);
 
 // Errors handling middlewares
 
